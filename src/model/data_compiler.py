6abcc502a120b8a10e543f5c3e08fc8d53f7e97c
Jason <jasoone@students.ubc.ca>
2024-05-17 16:48:15 -0700

    implemented prelimiary CNN model

--- src/model/data_compiler.py
+++ src/model/data_compiler.py
@@ -1,9 +1,9 @@
 import numpy as np
 import os
+from sklearn.utils import shuffle
+import matplotlib.pyplot as plt
 
-NUM_CLASSES = 345
-NUM_EXAMPLES_PER_CLASS = 2
-VECTOR_LENGTH = 784
+IMAGE_DIM = 28
 
 directory = os.getcwd() + "\\Quick-Draw-Guesser\\data\\draw_data"
 
@@ -13,20 +13,31 @@ def compile_data(num_classes, num_examples_per_class):
     matrix and corresponding target vector to feed into the CNN and shuffles it. 
     Additionally returns a string array of all class names.
     '''
-    X = np.zeros((num_classes * num_examples_per_class, VECTOR_LENGTH))
+    np.random.seed(1)
+    X = np.empty((num_classes * num_examples_per_class, IMAGE_DIM, IMAGE_DIM))
     y = np.zeros(num_classes * num_examples_per_class)
     classes = []
 
     for class_enum, file in enumerate(os.listdir(directory)):
+        if class_enum == num_classes - 1:
+            break
         filename = os.fsdecode(file)
         path = directory + "\\" + filename
         arr = np.load(path)
+        #print(filename)
         for i in range(num_examples_per_class):
-            X[class_enum * num_examples_per_class + i] = arr[i] / 255
+            X[class_enum * num_examples_per_class + i] = np.reshape(arr[i] / 255, (28, 28))
             y[class_enum * num_examples_per_class + i] = class_enum
             classes.append(filename[:-4])
 
-    # TODO: shuffle data
+    y = y.astype(int)
+    X, y = shuffle(X, y)
     return X, y, classes
 
-compile_data(NUM_CLASSES, NUM_EXAMPLES_PER_CLASS)
+# X, y, classes = compile_data(345, 1)
+# ex = X[0]
+# label = y[0]
+# print(classes[label])
+
+# plt.imshow(ex, cmap=plt.cm.binary)
+# plt.show()
--- src/model/model.py
+++ src/model/model.py
@@ -1,5 +1,50 @@
 import cv2 as cv
-import tensorflow as tf
+from tensorflow import keras 
+from keras import models, layers, regularizers, optimizers, losses, metrics
 import matplotlib as plt
 import IPython.display as display
 from data_compiler import compile_data
+
+NUM_CLASSES = 100
+NUM_EXAMPLES_PER_CLASS = 4000
+TOTAL_EXAMPLES = NUM_CLASSES * NUM_EXAMPLES_PER_CLASS
+
+TRAIN_PROP = 0.8
+TRAIN_IDX = int(TOTAL_EXAMPLES * TRAIN_PROP)
+
+X, y, classes = compile_data(NUM_CLASSES, NUM_EXAMPLES_PER_CLASS)
+X_train, y_train = X[:TRAIN_IDX], y[:TRAIN_IDX]
+X_testing, y_testing = X[TRAIN_IDX:], y[TRAIN_IDX:]
+test_split = len(X_testing)//2
+X_val, y_val = X_testing[:test_split], y_testing[:test_split]
+X_test, y_test = X_testing[test_split:], y_testing[test_split:]
+
+
+print("starting model")
+#model yields 0.8372 train accuracy, 0.7375 val accuracy
+model = models.Sequential()
+model.add(layers.Conv2D(64, (3, 3), activation='relu', input_shape=(28, 28, 1)))
+model.add(layers.BatchNormalization())
+model.add(layers.MaxPooling2D((2, 2)))
+
+model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+model.add(layers.BatchNormalization())
+model.add(layers.MaxPooling2D((2, 2)))
+
+model.add(layers.Conv2D(256, (3, 3), activation='relu'))
+model.add(layers.BatchNormalization())
+model.add(layers.MaxPooling2D((2, 2)))
+
+model.add(layers.Flatten())
+model.add(layers.Dense(256, activation='relu', kernel_regularizer=regularizers.L1L2()))
+
+model.add(layers.Dense(100, activation='softmax'))
+
+model.compile(optimizer='adam', 
+              loss='sparse_categorical_crossentropy', 
+              metrics=['accuracy'])
+model.fit(X_train, y_train, epochs=10, validation_data=(X_val, y_val))
+
+# To be used on final model
+#loss, accuracy = model.evaluate(X_test, y_test)
+#print(loss, accuracy)
